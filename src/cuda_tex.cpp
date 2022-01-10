475f330cf02433a67ea0fb454490a476b064cbb3
Nicolas Roussel <n.roussel13@gmail.com>
2022-01-10 10:26:50 +0100

    Texture: use staging aread when more than one CUDA texture is needed

--- src/cuda_tex.cpp
+++ src/cuda_tex.cpp
@@ -4,13 +4,16 @@
 #include "var.h"
 #include "op.h"
 #include <string.h>
+#include <memory>
+
+using StagingAreaDeleter = void (*)(void *);
 
 struct EnokiCudaTexture {
     size_t n_channels; /// Total number of channels
     size_t n_textures; // Number of texture objects
-    CUtexObject* textures; /// Array of CUDA texture objects
-    uint32_t* indices; /// Array of indices of texture object pointers for the JIT
-    CUarray* arrays; /// Array of CUDA arrays
+    std::unique_ptr<CUtexObject[]> textures; /// Array of CUDA texture objects
+    std::unique_ptr<uint32_t[]> indices; /// Array of indices of texture object pointers for the JIT
+    std::unique_ptr<CUarray[]> arrays; /// Array of CUDA arrays
 
     /**
      * \brief Construct a texture object for the given number of channels.
@@ -20,33 +23,24 @@ struct EnokiCudaTexture {
      * the given number of channels.
      */
     EnokiCudaTexture(size_t n_channels)
-        : n_channels(n_channels), n_textures(1 + ((n_channels - 1) / 4)) {
-        textures = new CUtexObject[n_textures];
-        indices = new uint32_t[n_textures];
-        arrays = new CUarray[n_textures];
-    }
-
-    ~EnokiCudaTexture() {
-        delete[] textures;
-        delete[] indices;
-        delete[] arrays;
-    }
+        : n_channels(n_channels), n_textures(1 + ((n_channels - 1) / 4)),
+          textures(std::make_unique<CUtexObject[]>(n_textures)),
+          indices(std::make_unique<uint32_t[]>(n_textures)),
+          arrays(std::make_unique<CUarray[]>(n_textures)) {}
 
     /**
      * \brief Returns the number of channels from the original data that are
      * associated with the texture object at the given index in \ref textures
      */
     size_t channels(size_t index) const {
-        if (index >= n_textures) {
+        if (index >= n_textures)
             jitc_raise("EnokiCudaTexture::channels(): invalid texture index!");
-        }
 
         size_t tex_channels = 4;
         if (index == n_textures - 1) {
             tex_channels = n_channels % 4;
-            if (tex_channels == 0) {
+            if (tex_channels == 0)
                 tex_channels = 4;
-            }
         }
 
         return tex_channels;
@@ -56,9 +50,9 @@ struct EnokiCudaTexture {
      * \brief Returns the number of channels (always either 1, 2 or 4) used
      * internally by the texture object at the given index in \ref textures
      */
-    size_t channels_int(size_t index) const {
+    size_t channels_internal(size_t index) const {
         const size_t channels_raw = channels(index);
-        return channels_raw == 3 ? 4 : channels_raw;
+        return (channels_raw == 3) ? 4 : channels_raw;
     }
 };
 
@@ -118,7 +112,7 @@ void *jitc_cuda_tex_create(size_t ndim, const size_t *shape, size_t n_channels,
 
     EnokiCudaTexture *texture = new EnokiCudaTexture(n_channels);
     for (size_t tex = 0; tex < texture->n_textures; ++tex) {
-        const size_t tex_channels = texture->channels_int(tex);
+        const size_t tex_channels = texture->channels_internal(tex);
 
         CUarray array = nullptr;
         if (ndim == 1 || ndim == 2) {
@@ -160,6 +154,54 @@ void *jitc_cuda_tex_create(size_t ndim, const size_t *shape, size_t n_channels,
     return (void *) texture;
 }
 
+static std::unique_ptr<void, StagingAreaDeleter>
+jitc_cuda_tex_alloc_staging_area(size_t n_texels,
+                                 const EnokiCudaTexture &texture) {
+    // Each texture except for the last one will need exactly 4 channels
+    size_t staging_area_size =
+        sizeof(float) * n_texels *
+        ((texture.n_textures - 1) * 4 +
+         texture.channels_internal(texture.n_textures - 1));
+    void *staging_area = jitc_malloc(AllocType::Device, staging_area_size);
+
+    return std::unique_ptr<void, StagingAreaDeleter>(staging_area, jitc_free);
+}
+
+/*
+ * \brief Copy texture to a staging area that is pitched over the channels, such
+ * that the data for each underlying CUDA texture is partitioned.
+ */
+static void jitc_cuda_tex_memcpy_d2s(
+    ThreadState *ts,
+    const std::unique_ptr<void, StagingAreaDeleter> &staging_area,
+    size_t n_texels, const void *src_ptr, const EnokiCudaTexture &dst_texture) {
+    scoped_set_context guard(ts->context);
+
+    size_t texel_size = dst_texture.n_channels * sizeof(float);
+
+    CUDA_MEMCPY2D op;
+    memset(&op, 0, sizeof(CUDA_MEMCPY2D));
+
+    for (size_t tex = 0; tex < dst_texture.n_textures; ++tex) {
+        size_t texel_offset = tex * 4 * sizeof(float);
+
+        op.srcXInBytes = texel_offset;
+        op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
+        op.srcDevice = (CUdeviceptr) src_ptr;
+        op.srcPitch = texel_size;
+
+        op.dstXInBytes = n_texels * texel_offset;
+        op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+        op.dstDevice = (CUdeviceptr) staging_area.get();
+        op.dstPitch = dst_texture.channels_internal(tex) * sizeof(float);
+
+        op.WidthInBytes = dst_texture.channels(tex) * sizeof(float);
+        op.Height = n_texels;
+
+        cuda_check(cuMemcpy2DAsync(&op, ts->stream));
+    }
+}
+
 void jitc_cuda_tex_memcpy_d2t(size_t ndim, const size_t *shape,
                               const void *src_ptr, void *dst_texture_handle) {
     if (ndim < 1 || ndim > 3)
@@ -168,56 +210,107 @@ void jitc_cuda_tex_memcpy_d2t(size_t ndim, const size_t *shape,
     ThreadState *ts = thread_state(JitBackend::CUDA);
     scoped_set_context guard(ts->context);
 
-    EnokiCudaTexture *dst_texture = (EnokiCudaTexture *) dst_texture_handle;
+    EnokiCudaTexture &dst_texture = *((EnokiCudaTexture *) dst_texture_handle);
 
-    for (size_t tex = 0; tex < dst_texture->n_textures; ++tex) {
-        size_t entry_width_in_bytes = dst_texture->n_channels * sizeof(float);
-        size_t entry_offset = tex * 4 * sizeof(float);
-        size_t pitch = shape[0] * dst_texture->n_channels * sizeof(float);
+    size_t n_texels = shape[0];
+    for (size_t dim = 1; dim < ndim; ++dim)
+        n_texels *= shape[dim];
 
-        if (ndim == 1 || ndim == 2) {
-            for (size_t x = 0; x < shape[0]; ++x) {
-                CUDA_MEMCPY2D op;
-                memset(&op, 0, sizeof(CUDA_MEMCPY2D));
+    StagingAreaDeleter noop = [](void *) {};
+    std::unique_ptr<void, StagingAreaDeleter> staging_area(nullptr, noop);
+    bool needs_staging_area =
+        (dst_texture.n_textures > 1) || (dst_texture.n_channels == 3);
+    if (needs_staging_area) {
+        staging_area = jitc_cuda_tex_alloc_staging_area(n_texels, dst_texture);
+        jitc_cuda_tex_memcpy_d2s(ts, staging_area, n_texels, src_ptr,
+                                 dst_texture);
+    }
 
-                op.srcXInBytes = x * entry_width_in_bytes + entry_offset;
-                op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
-                op.srcDevice = (CUdeviceptr) src_ptr;
-                op.srcPitch = pitch;
+    if (ndim == 1 || ndim == 2) {
+        CUDA_MEMCPY2D op;
+        memset(&op, 0, sizeof(CUDA_MEMCPY2D));
 
-                op.dstXInBytes =
-                    x * dst_texture->channels_int(tex) * sizeof(float);
-                op.dstMemoryType = CU_MEMORYTYPE_ARRAY;
-                op.dstArray = dst_texture->arrays[tex];
+        for (size_t tex = 0; tex < dst_texture.n_textures; ++tex) {
+            size_t pitch =
+                shape[0] * dst_texture.channels_internal(tex) * sizeof(float);
 
-                op.WidthInBytes = dst_texture->channels(tex) * sizeof(float);
-                op.Height = (ndim == 2) ? shape[1] : 1;
-
-                cuda_check(cuMemcpy2DAsync(&op, ts->stream));
+            op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
+            op.srcDevice = (CUdeviceptr) src_ptr;
+            op.srcPitch = pitch;
+            if (needs_staging_area) {
+                op.srcDevice = (CUdeviceptr) staging_area.get();
+                op.srcXInBytes = tex * n_texels * 4 * sizeof(float);
             }
-        } else {
-            for (size_t x = 0; x < shape[0]; ++x) {
-                CUDA_MEMCPY3D op;
-                memset(&op, 0, sizeof(CUDA_MEMCPY3D));
 
-                op.srcXInBytes = x * entry_width_in_bytes + entry_offset;
-                op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
-                op.srcDevice = (CUdeviceptr) src_ptr;
-                op.srcPitch = pitch;
-                op.srcHeight = shape[1];
+            op.dstMemoryType = CU_MEMORYTYPE_ARRAY;
+            op.dstArray = dst_texture.arrays[tex];
 
-                op.dstXInBytes =
-                    x * dst_texture->channels_int(tex) * sizeof(float);
-                op.dstMemoryType = CU_MEMORYTYPE_ARRAY;
-                op.dstArray = dst_texture->arrays[tex];
+            op.WidthInBytes = pitch;
+            op.Height = (ndim == 2) ? shape[1] : 1;
 
-                op.WidthInBytes = dst_texture->channels(tex) * sizeof(float);
-                op.Height = shape[1];
-                op.Depth = shape[2];
-
-                cuda_check(cuMemcpy3DAsync(&op, ts->stream));
-            }
+            cuda_check(cuMemcpy2DAsync(&op, ts->stream));
         }
+    } else {
+        CUDA_MEMCPY3D op;
+        memset(&op, 0, sizeof(CUDA_MEMCPY2D));
+
+        for (size_t tex = 0; tex < dst_texture.n_textures; ++tex) {
+            size_t pitch =
+                shape[0] * dst_texture.channels_internal(tex) * sizeof(float);
+
+            op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
+            op.srcDevice = (CUdeviceptr) src_ptr;
+            op.srcPitch = pitch;
+            op.srcHeight = shape[1];
+            if (needs_staging_area) {
+                op.srcXInBytes = tex * n_texels * 4 * sizeof(float);
+                op.srcDevice = (CUdeviceptr) staging_area.get();
+            }
+
+            op.dstMemoryType = CU_MEMORYTYPE_ARRAY;
+            op.dstArray = dst_texture.arrays[tex];
+
+            op.WidthInBytes = pitch;
+            op.Height = shape[1];
+            op.Depth = shape[2];
+
+            cuda_check(cuMemcpy3DAsync(&op, ts->stream));
+        }
+    }
+}
+
+/*
+ * \brief Copy texture from a staging area that is pitched over the channels,
+ * such that the data for each underlying CUDA texture is partitioned.
+ */
+static void jitc_cuda_tex_memcpy_s2d(
+    ThreadState *ts,
+    const std::unique_ptr<void, StagingAreaDeleter> &staging_area,
+    size_t n_texels, const void *dst_ptr, const EnokiCudaTexture &src_texture) {
+    scoped_set_context guard(ts->context);
+
+    size_t texel_size = src_texture.n_channels * sizeof(float);
+
+    CUDA_MEMCPY2D op;
+    memset(&op, 0, sizeof(CUDA_MEMCPY2D));
+
+    for (size_t tex = 0; tex < src_texture.n_textures; ++tex) {
+        size_t texel_offset = tex * 4 * sizeof(float);
+
+        op.srcXInBytes = n_texels * texel_offset;
+        op.srcMemoryType = CU_MEMORYTYPE_DEVICE;
+        op.srcDevice = (CUdeviceptr) staging_area.get();
+        op.srcPitch = src_texture.channels_internal(tex) * sizeof(float);
+
+        op.dstXInBytes = texel_offset;
+        op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+        op.dstDevice = (CUdeviceptr) dst_ptr;
+        op.dstPitch = texel_size;
+
+        op.WidthInBytes = src_texture.channels(tex) * sizeof(float);
+        op.Height = n_texels;
+
+        cuda_check(cuMemcpy2DAsync(&op, ts->stream));
     }
 }
 
@@ -229,57 +322,75 @@ void jitc_cuda_tex_memcpy_t2d(size_t ndim, const size_t *shape,
     ThreadState *ts = thread_state(JitBackend::CUDA);
     scoped_set_context guard(ts->context);
 
-    EnokiCudaTexture *src_texture = (EnokiCudaTexture *) src_texture_handle;
+    EnokiCudaTexture &src_texture = *((EnokiCudaTexture *) src_texture_handle);
 
-    for (size_t tex = 0; tex < src_texture->n_textures; ++tex) {
-        size_t entry_width_in_bytes = src_texture->n_channels * sizeof(float);
-        size_t entry_offset = tex * 4 * sizeof(float);
-        size_t pitch = shape[0] * src_texture->n_channels * sizeof(float);
+    size_t n_texels = shape[0];
+    for (size_t dim = 1; dim < ndim; ++dim)
+        n_texels *= shape[dim];
 
-        if (ndim == 1 || ndim == 2) {
-            for (size_t x = 0; x < shape[0]; ++x) {
-                CUDA_MEMCPY2D op;
-                memset(&op, 0, sizeof(CUDA_MEMCPY2D));
+    auto noop = [](void *) {};
+    std::unique_ptr<void, StagingAreaDeleter> staging_area(nullptr, noop);
+    bool needs_staging_area =
+        (src_texture.n_textures > 1) || (src_texture.n_channels == 3);
+    if (needs_staging_area) {
+        staging_area = jitc_cuda_tex_alloc_staging_area(n_texels, src_texture);
+    }
 
-                op.srcXInBytes =
-                    x * src_texture->channels_int(tex) * sizeof(float);
-                op.srcMemoryType = CU_MEMORYTYPE_ARRAY;
-                op.srcArray = src_texture->arrays[tex];
+    if (ndim == 1 || ndim == 2) {
+        CUDA_MEMCPY2D op;
+        memset(&op, 0, sizeof(CUDA_MEMCPY2D));
 
-                op.dstXInBytes = x * entry_width_in_bytes + entry_offset;
-                op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
-                op.dstDevice = (CUdeviceptr) dst_ptr;
-                op.dstPitch = pitch;
+        for (size_t tex = 0; tex < src_texture.n_textures; ++tex) {
+            size_t pitch =
+                shape[0] * src_texture.channels_internal(tex) * sizeof(float);
 
-                op.WidthInBytes = src_texture->channels(tex) * sizeof(float);
-                op.Height = (ndim == 2) ? shape[1] : 1;
+            op.srcMemoryType = CU_MEMORYTYPE_ARRAY;
+            op.srcArray = src_texture.arrays[tex];
 
-                cuda_check(cuMemcpy2DAsync(&op, ts->stream));
+            op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+            op.dstDevice = (CUdeviceptr) dst_ptr;
+            op.dstPitch = pitch;
+            if (needs_staging_area) {
+                op.dstXInBytes = tex * n_texels * 4 * sizeof(float);
+                op.dstDevice = (CUdeviceptr) staging_area.get();
             }
-        } else {
-            for (size_t x = 0; x < shape[0]; ++x) {
-                CUDA_MEMCPY3D op;
-                memset(&op, 0, sizeof(CUDA_MEMCPY3D));
 
-                op.srcXInBytes =
-                    x * src_texture->channels_int(tex) * sizeof(float);
-                op.srcMemoryType = CU_MEMORYTYPE_ARRAY;
-                op.srcArray = src_texture->arrays[tex];
+            op.WidthInBytes = pitch;
+            op.Height = (ndim == 2) ? shape[1] : 1;
 
-                op.dstXInBytes = x * entry_width_in_bytes + entry_offset;
-                op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
-                op.dstDevice = (CUdeviceptr) dst_ptr;
-                op.dstPitch = pitch;
-                op.dstHeight = shape[1];
+            cuda_check(cuMemcpy2DAsync(&op, ts->stream));
+        }
+    } else {
+        CUDA_MEMCPY3D op;
+        memset(&op, 0, sizeof(CUDA_MEMCPY3D));
 
-                op.WidthInBytes = src_texture->channels(tex) * sizeof(float);
-                op.Height = shape[1];
-                op.Depth = shape[2];
+        for (size_t tex = 0; tex < src_texture.n_textures; ++tex) {
+            size_t pitch =
+                shape[0] * src_texture.channels_internal(tex) * sizeof(float);
 
-                cuda_check(cuMemcpy3DAsync(&op, ts->stream));
+            op.srcMemoryType = CU_MEMORYTYPE_ARRAY;
+            op.srcArray = src_texture.arrays[tex];
+
+            op.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+            op.dstDevice = (CUdeviceptr) dst_ptr;
+            op.dstPitch = pitch;
+            op.dstHeight = shape[1];
+            if (needs_staging_area) {
+                op.dstXInBytes = tex * n_texels * 4 * sizeof(float);
+                op.dstDevice = (CUdeviceptr) staging_area.get();
             }
+
+            op.WidthInBytes = pitch;
+            op.Height = shape[1];
+            op.Depth = shape[2];
+
+            cuda_check(cuMemcpy3DAsync(&op, ts->stream));
         }
     }
+
+    if (needs_staging_area)
+        jitc_cuda_tex_memcpy_s2d(ts, staging_area, n_texels, dst_ptr,
+                                 src_texture);
 }
 
 void jitc_cuda_tex_lookup(size_t ndim, const void *texture_handle,
