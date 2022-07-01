b14d8f083e3e831a5c89577759079e2bd3c77095
Sebastien Speierer <sebastieneps@gmail.com>
2022-07-01 09:33:01 +0200

    Fix MSVC warnings

--- src/optix_api.cpp
+++ src/optix_api.cpp
@@ -531,7 +531,7 @@ bool jitc_optix_compile(ThreadState *ts, const char *buf, size_t buf_size,
     }
 
     std::function<void(OptixTask)> execute_task = [&](OptixTask task) {
-        size_t max_new_tasks = pool_size();
+        unsigned int max_new_tasks = pool_size();
 
         std::unique_ptr<OptixTask[]> new_tasks =
             std::make_unique<OptixTask[]>(max_new_tasks);
