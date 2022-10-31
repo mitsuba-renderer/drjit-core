54ac6f5baf52eccca761c3981e97b78de648eb9a
Tom Kneiphof <tomkneiphof@gmail.com>
2022-10-31 15:19:55 +0100

    Fix unknown variable bug with nested loops

--- src/vcall.cpp
+++ src/vcall.cpp
@@ -1319,6 +1319,10 @@ static void jitc_var_vcall_assemble_llvm(
 void jitc_var_vcall_collect_data(tsl::robin_map<uint64_t, uint32_t, UInt64Hasher> &data_map,
                                  uint32_t &data_offset, uint32_t inst_id,
                                  uint32_t index, bool &use_self) {
+    // Skip if `index` refers to an invalid variable.
+    // NOTE: index should be refactored to something like `id`.
+    if (index == 0)
+        return;
     uint64_t key = (uint64_t) index + (((uint64_t) inst_id) << 32);
     auto it_and_status = data_map.emplace(key, (uint32_t) -1);
     if (!it_and_status.second)
