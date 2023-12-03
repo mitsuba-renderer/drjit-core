c0a8423cf04ddefcfedd4826f5d63078c225f637
Wenzel Jakob <wenzel.jakob@epfl.ch>
2023-12-03 08:45:36 +0100

    Improve potentially buggy ``jitc_mkperm()`` task waiting logic
    There have been some reports/suspicions by Miguel that a bug might be
    lingering in ``jitc_mkperm()``, so I'm using this opportunity to
    improve the task waiting/reference counting logic.
    
    Instead of asking the ``jitc_submit_cpu()`` to not release a reference
    to the preceding task, this commit uses ``task_retain`` to immediately
    acquire a stable reference that it can wait on later.

--- src/util.cpp
+++ src/util.cpp
@@ -1075,6 +1075,7 @@ uint32_t jitc_mkperm(JitBackend backend, const uint32_t *ptr, uint32_t size,
         );
 
         Task *local_task = jitc_task;
+        task_retain(local_task);
 
         // Phase 2
         jitc_submit_cpu(
@@ -1094,7 +1095,7 @@ uint32_t jitc_mkperm(JitBackend backend, const uint32_t *ptr, uint32_t size,
                 free(buckets_local);
             },
 
-            size, blocks, false
+            size, blocks
         );
 
         // Free memory (happens asynchronously after the above stmt.)
