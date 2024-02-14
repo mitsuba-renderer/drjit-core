mitsuba-renderer/drjit-core

commit 12a2b8bf9d71fda56e73076b620946d65b566842

Switch to ``cuModuleLoadDataEx()`` for compiling and loading PTX code
Dr.Jit previously used the ``cuLink*`` family of functions to compile
PTX code into CUBIN files, which it then cached in the ``~/.drjit``
directory. It turns out that this API has an unexpected side effect: it
generates *relocatable device code* via an implicit ``-rdc=true``
argument to NVIDIA's PTX-to-SASS JIT compiler.

This is very bad: it causes a significant number of spills and generally
suboptimal code generation whenever Dr.Jit compiles kernels that include
additional subroutines (e.g. implementation details of
``dr.scatter_reduce()``, vectorized function calls, etc.). This problem
arises in plain CUDA kernels---in particular, the OptiX code path is
unaffected.

Simply by switching to another compilation API without this behavior, a
benchmark kernel from Baptiste Nicolet's current research project goes
from 343 ms to 222 ms!

Further discussion is available here:
https://forums.developer.nvidia.com/t/ptx-jit-spills-registers-in-trivial-programs/279620

I've switched everything over to use ``cuModuleLoadDataEx()`` in favor
of ``cuLink*``. A consequence of this change is that we now have to rely
on CUDA's builtin kernel caching scheme. In other words, Dr.Jit will (at
least for now) no longer generate CUDA-specific files in the
``~/.drjit`` directory. Clearing the kernel cache then requires wiping
the ``~/.nv/ComputeCache`` directory.

src/cuda.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "cuda_api.h"
+#include <utility>
 
 /// Major version of the detected CUDA version
 extern int jitc_cuda_version_major;
@@ -37,8 +38,8 @@ extern void *jitc_cuda_lookup(const char *name);
 
 struct Kernel;
 
-/// Compile an IR string
-extern void jitc_cuda_compile(const char *str, size_t size, Kernel &kernel);
+/// Compile an PTX string. Returns the resulting module and a cache hit true/false flag
+extern std::pair<CUmodule, bool> jitc_cuda_compile(const char *str);
 
 /// Assert that a CUDA operation is correctly issued
 #define cuda_check(err) cuda_check_impl(err, __FILE__, __LINE__)

src/cuda_core.cpp
@@ -39,9 +39,11 @@ CUfunction *jitc_cuda_reductions[(int) ReduceOp::Count]
                                 [(int) VarType::Count] = { };
 CUfunction *jitc_cuda_aggregate = nullptr;
 
-void jitc_cuda_compile(const char *buf, size_t buf_size, Kernel &kernel) {
+std::pair<CUmodule, bool> jitc_cuda_compile(const char *buf) {
     const uintptr_t log_size = 16384;
     char error_log[log_size], info_log[log_size];
+	info_log[0] = '\0';
+	error_log[0] = '\0';
 
     CUjit_option arg[] = {
         CU_JIT_OPTIMIZATION_LEVEL,
@@ -65,32 +67,37 @@ void jitc_cuda_compile(const char *buf, size_t buf_size, Kernel &kernel) {
         (void *) 0
     };
 
-    CUlinkState link_state;
-    cuda_check(cuLinkCreate(sizeof(argv) / sizeof(void *), arg, argv, &link_state));
+	size_t nargs = sizeof(arg) / sizeof(CUjit_option);
 
-    int rt = cuLinkAddData(link_state, CU_JIT_INPUT_PTX, (void *) buf,
-                           buf_size, nullptr, 0, nullptr, nullptr);
-    if (rt != CUDA_SUCCESS)
-        jitc_fail("jit_cuda_compile(): compilation failed. Please see the PTX "
-                  "assembly listing and error message below:\n\n%s\n\n%s",
-                  buf, error_log);
+	CUmodule mod = nullptr;
+	CUresult rv = 0;
 
-    void *link_output = nullptr;
-    size_t link_output_size = 0;
-    cuda_check(cuLinkComplete(link_state, &link_output, &link_output_size));
-    if (rt != CUDA_SUCCESS)
-        jitc_fail("jit_cuda_compile(): compilation failed. Please see the PTX "
-                  "assembly listing and error message below:\n\n%s\n\n%s",
-                  buf, error_log);
+	for (int i = 0; i < 2; ++i) {
+		{
+			unlock_guard guard(state.lock);
+            rv = cuModuleLoadDataEx(&mod, buf, nargs, arg, argv);
+        }
 
-    jitc_log(Trace, "Detailed linker output:\n%s", info_log);
+		if (rv == CUDA_ERROR_OUT_OF_MEMORY) {
+			if (i == 0)
+                jitc_flush_malloc_cache(true);
+			else
+				cuda_check(rv);
+		} else {
+			break;
+		}
+	}
 
-    kernel.data = malloc_check(link_output_size);
-    kernel.size = (uint32_t) link_output_size;
-    memcpy(kernel.data, link_output, link_output_size);
+	if (rv != CUDA_SUCCESS)
+		jitc_fail("jit_cuda_compile(): compilation failed. Please see the PTX "
+				  "assembly listing and error message below:\n\n%s\n\n%s",
+				  buf, error_log);
 
-    // Destroy the linker invocation
-    cuda_check(cuLinkDestroy(link_state));
+	bool cache_hit = info_log[0] == '\0';
+	if (!cache_hit)
+		jitc_log(Trace, "Detailed linker output:\n%s", info_log);
+
+    return { mod, cache_hit };
 }
 
 void cuda_check_impl(CUresult errval, const char *file, const int line) {
@@ -224,9 +231,6 @@ bool jitc_cuda_init() {
                                                  : kernels_50_size_uncompressed;
         int kernels_size_compressed   = cc >= 70 ? kernels_70_size_compressed
                                                  : kernels_50_size_compressed;
-        XXH128_hash_t kernels_hash;
-        kernels_hash.low64  = cc >= 70 ? kernels_70_hash_low64  : kernels_50_hash_low64;
-        kernels_hash.high64 = cc >= 70 ? kernels_70_hash_high64 : kernels_50_hash_high64;
 
         // Decompress the supplemental PTX content
         char *uncompressed =
@@ -242,25 +246,9 @@ bool jitc_cuda_init() {
 
         uncompressed_ptx[kernels_size_uncompressed] = '\0';
 
-        hash_combine((size_t &) kernels_hash.low64, (size_t) cc);
-        hash_combine((size_t &) kernels_hash.high64, (size_t) cc);
-
-        Kernel kernel;
-        if (!jitc_kernel_load(uncompressed_ptx, kernels_size_uncompressed,
-                              JitBackend::CUDA, kernels_hash, kernel)) {
-            jitc_cuda_compile(uncompressed_ptx, kernels_size_uncompressed,
-                              kernel);
-            jitc_kernel_write(uncompressed_ptx, kernels_size_uncompressed,
-                              JitBackend::CUDA, kernels_hash, kernel);
-        }
-
+		CUmodule m = jitc_cuda_compile(uncompressed_ptx).first;
+		jitc_cuda_module[i] = m;
         free(uncompressed);
-
-        // .. and register it with CUDA
-        CUmodule m;
-        cuda_check(cuModuleLoadData(&m, kernel.data));
-        free(kernel.data);
-        jitc_cuda_module[i] = m;
 
         #define LOAD(name)                                                       \
             if (i == 0)                                                          \

src/cuda_api.cpp
@@ -101,10 +101,6 @@ bool jitc_cuda_api_init() {
         LOAD(cuInit);
         LOAD(cuLaunchHostFunc);
         LOAD(cuLaunchKernel);
-        LOAD(cuLinkAddData, "v2");
-        LOAD(cuLinkComplete);
-        LOAD(cuLinkCreate, "v2");
-        LOAD(cuLinkDestroy);
         LOAD(cuMemAdvise);
         LOAD(cuMemAlloc, "v2");
         LOAD(cuMemAllocHost, "v2");
@@ -118,6 +114,7 @@ bool jitc_cuda_api_init() {
         LOAD(cuMemsetD8Async);
         LOAD(cuModuleGetFunction);
         LOAD(cuModuleLoadData);
+        LOAD(cuModuleLoadDataEx);
         LOAD(cuModuleUnload);
         LOAD(cuOccupancyMaxPotentialBlockSize);
         LOAD(cuCtxPushCurrent, "v2");
@@ -168,11 +165,10 @@ void jitc_cuda_api_shutdown() {
     Z(cuEventCreate); Z(cuEventDestroy); Z(cuEventRecord);
     Z(cuEventSynchronize); Z(cuEventElapsedTime); Z(cuFuncSetAttribute);
     Z(cuGetErrorName); Z(cuGetErrorString); Z(cuInit); Z(cuLaunchHostFunc);
-    Z(cuLaunchKernel); Z(cuLinkAddData); Z(cuLinkComplete); Z(cuLinkCreate);
-    Z(cuLinkDestroy); Z(cuMemAdvise); Z(cuMemAlloc); Z(cuMemAllocHost);
+    Z(cuLaunchKernel); Z(cuMemAdvise); Z(cuMemAlloc); Z(cuMemAllocHost);
     Z(cuMemFree); Z(cuMemFreeHost); Z(cuMemcpy); Z(cuMemcpyAsync);
     Z(cuMemsetD16Async); Z(cuMemsetD32Async); Z(cuMemsetD8Async);
-    Z(cuModuleGetFunction); Z(cuModuleLoadData); Z(cuModuleUnload);
+    Z(cuModuleGetFunction); Z(cuModuleLoadData); Z(cuModuleLoadDataEx); Z(cuModuleUnload);
     Z(cuOccupancyMaxPotentialBlockSize); Z(cuCtxPushCurrent);
     Z(cuCtxPopCurrent); Z(cuStreamCreate); Z(cuStreamDestroy);
     Z(cuStreamSynchronize); Z(cuStreamWaitEvent); Z(cuPointerGetAttribute);

src/cuda_api.h
@@ -44,7 +44,6 @@ extern void jitc_cuda_sync_stream(uintptr_t stream);
 #  define CU_FUNC_ATTRIBUTE_PREFERRED_SHARED_MEMORY_CARVEOUT 9
 #  define CU_FUNC_CACHE_PREFER_L1 2
 
-#  define CU_JIT_INPUT_PTX 1
 #  define CU_JIT_INFO_LOG_BUFFER 3
 #  define CU_JIT_INFO_LOG_BUFFER_SIZE_BYTES 4
 #  define CU_JIT_ERROR_LOG_BUFFER 5
@@ -236,11 +235,6 @@ DR_CUDA_SYM(CUresult (*cuLaunchKernel)(CUfunction f, unsigned int, unsigned int,
                                        unsigned int, unsigned int, unsigned int,
                                        unsigned int, unsigned int, CUstream, void **,
                                        void **));
-DR_CUDA_SYM(CUresult (*cuLinkAddData)(CUlinkState, int, void *, size_t, const char *,
-                                      unsigned int, int *, void **));
-DR_CUDA_SYM(CUresult (*cuLinkComplete)(CUlinkState, void **, size_t *));
-DR_CUDA_SYM(CUresult (*cuLinkCreate)(unsigned int, int *, void **, CUlinkState *));
-DR_CUDA_SYM(CUresult (*cuLinkDestroy)(CUlinkState));
 DR_CUDA_SYM(CUresult (*cuPointerGetAttribute)(void* data, int, void*));
 DR_CUDA_SYM(CUresult (*cuMemAdvise)(void *, size_t, int, CUdevice));
 DR_CUDA_SYM(CUresult (*cuMemAlloc)(void **, size_t));
@@ -254,6 +248,7 @@ DR_CUDA_SYM(CUresult (*cuMemsetD32Async)(void *, unsigned int, size_t, CUstream)
 DR_CUDA_SYM(CUresult (*cuMemsetD8Async)(void *, unsigned char, size_t, CUstream));
 DR_CUDA_SYM(CUresult (*cuModuleGetFunction)(CUfunction *, CUmodule, const char *));
 DR_CUDA_SYM(CUresult (*cuModuleLoadData)(CUmodule *, const void *));
+DR_CUDA_SYM(CUresult (*cuModuleLoadDataEx)(CUmodule *, const void *, unsigned int, CUjit_option*, void**));
 DR_CUDA_SYM(CUresult (*cuModuleUnload)(CUmodule));
 DR_CUDA_SYM(CUresult (*cuOccupancyMaxPotentialBlockSize)(int *, int *, CUfunction,
                                                          void *, size_t, int));

src/eval.cpp
@@ -404,7 +404,6 @@ void jitc_assemble(ThreadState *ts, ScheduledGroup group) {
 }
 
 static ProfilerRegion profiler_region_backend_compile("jit_eval: compiling");
-static ProfilerRegion profiler_region_backend_load("jit_eval: loading");
 
 Task *jitc_run(ThreadState *ts, ScheduledGroup group) {
     uint64_t flags = 0;
@@ -431,51 +430,32 @@ Task *jitc_run(ThreadState *ts, ScheduledGroup group) {
     if (it == state.kernel_cache.end()) {
         bool cache_hit = false;
 
-        if (!uses_optix)
+        if (ts->backend == JitBackend::CUDA) {
+			ProfilerPhase profiler(profiler_region_backend_compile);
+			if (!uses_optix) {
+				kernel.size = 1; // dummy size value to distinguish between OptiX and CUDA kernels
+				kernel.data = nullptr;
+				std::tie(kernel.cuda.mod, cache_hit) = jitc_cuda_compile(buffer.get());
+			} else {
+				#if defined(DRJIT_ENABLE_OPTIX)
+					cache_hit = jitc_optix_compile(
+						ts, buffer.get(), buffer.size(), kernel_name, kernel);
+				#endif
+			}
+		} else {
             cache_hit = jitc_kernel_load(buffer.get(), (uint32_t) buffer.size(),
                                          ts->backend, kernel_hash, kernel);
 
-        if (!cache_hit) {
-            ProfilerPhase profiler(profiler_region_backend_compile);
-            if (ts->backend == JitBackend::CUDA) {
-                if (!uses_optix) {
-                    jitc_cuda_compile(buffer.get(), buffer.size(), kernel);
-                } else {
-#if defined(DRJIT_ENABLE_OPTIX)
-                    cache_hit = jitc_optix_compile(
-                        ts, buffer.get(), buffer.size(), kernel_name, kernel);
-#else
-                    jitc_fail("jit_run(): OptiX support was not enabled in DrJit.");
-#endif
-                }
-            } else {
-                jitc_llvm_compile(kernel);
-            }
-
-            if (kernel.data)
+			if (!cache_hit) {
+				ProfilerPhase profiler(profiler_region_backend_compile);
+				jitc_llvm_compile(kernel);
                 jitc_kernel_write(buffer.get(), (uint32_t) buffer.size(),
                                   ts->backend, kernel_hash, kernel);
-        }
+				jitc_llvm_disasm(kernel);
+			}
+		}
 
-        ProfilerPhase profiler(profiler_region_backend_load);
-
-        if (ts->backend == JitBackend::LLVM) {
-            jitc_llvm_disasm(kernel);
-        } else if (!uses_optix) {
-            CUresult ret = (CUresult) 0;
-            /* Unlock while synchronizing */ {
-                unlock_guard guard(state.lock);
-                ret = cuModuleLoadData(&kernel.cuda.mod, kernel.data);
-            }
-            if (ret == CUDA_ERROR_OUT_OF_MEMORY) {
-                jitc_flush_malloc_cache(true);
-                /* Unlock while synchronizing */ {
-                    unlock_guard guard(state.lock);
-                    ret = cuModuleLoadData(&kernel.cuda.mod, kernel.data);
-                }
-            }
-            cuda_check(ret);
-
+        if (ts->backend == JitBackend::CUDA && !uses_optix) {
             // Locate the kernel entry point
             size_t offset = buffer.size();
             buffer.fmt_cuda(2, "drjit_$Q$Q", kernel_hash.high64,
@@ -497,9 +477,6 @@ Task *jitc_run(ThreadState *ts, ScheduledGroup group) {
             cuda_check(cuFuncSetAttribute(
                 kernel.cuda.func, CU_FUNC_ATTRIBUTE_PREFERRED_SHARED_MEMORY_CARVEOUT,
                 CU_SHAREDMEM_CARVEOUT_MAX_L1));
-
-            free(kernel.data);
-            kernel.data = nullptr;
         }
 
         float link_time = timer();

src/optix_core.cpp
@@ -369,10 +369,6 @@ bool jitc_optix_compile(ThreadState *ts, const char *buf, size_t buf_size,
 #endif
 
     size_t size_before = pipeline.program_groups.size();
-#if 0
-    OptixStackSizes stack_sizes;
-    unsigned int cssRG = 0, dssDC = 0;
-#endif
 
     for (uint32_t i = 0; i < n_programs; ++i) {
         if (i == 0)
@@ -380,14 +376,6 @@ bool jitc_optix_compile(ThreadState *ts, const char *buf, size_t buf_size,
         else
             free((char *) pgd[i].callables.entryFunctionNameDC);
         pipeline.program_groups.push_back(kernel.optix.pg[i]);
-#if 0
-        jitc_optix_check(optixProgramGroupGetStackSize(
-            pipeline.program_groups[i], &stack_sizes));
-        if (i == 0)
-            cssRG = stack_sizes.cssRG;
-        else
-            dssDC = std::max(dssDC, stack_sizes.dssDC);
-#endif
     }
 
     log_size = sizeof(error_log);
@@ -431,7 +419,7 @@ bool jitc_optix_compile(ThreadState *ts, const char *buf, size_t buf_size,
         jitc_log(Error, "jit_optix_compile(): an OptiX program is using "
                         "continuous callables which is not supported by Dr.Jit!");
 
-    unsigned int max_dc_depth = 2; // Support nested Calls
+    unsigned int max_dc_depth = 2; // Support nested calls
     unsigned int dc_stack_size_from_traversal = 0; // DC is not invoked from IS or AH.
     unsigned int dc_stack_size_from_state = max_dc_depth * ssp.dssDC; // DC is invoked from RG, MS, or CH.
     unsigned int continuation_stack_size = ssp.cssRG + std::max(std::max(ssp.cssCH, ssp.cssMS), ssp.cssAH + ssp.cssIS);

commit 3d8d1aefae4c34507dca882e8591ab93cea50e60

Fix race condition in `jit_init`
It was introduced in 12a2b8b.
`jit_init` calls `jitc_cuda_compile`, which now locally unlocks the state lock.
This meant that operations such as `jitc_init_thread_state` could start executing before `jit_init` would complete.

src/cuda.h
@@ -39,7 +39,8 @@ extern void *jitc_cuda_lookup(const char *name);
 struct Kernel;
 
 /// Compile an PTX string. Returns the resulting module and a cache hit true/false flag
-extern std::pair<CUmodule, bool> jitc_cuda_compile(const char *str);
+extern std::pair<CUmodule, bool>
+jitc_cuda_compile(const char *str, bool release_state_lock = true);
 
 /// Assert that a CUDA operation is correctly issued
 #define cuda_check(err) cuda_check_impl(err, __FILE__, __LINE__)

src/cuda_core.cpp
@@ -39,11 +39,11 @@ CUfunction *jitc_cuda_reductions[(int) ReduceOp::Count]
                                 [(int) VarType::Count] = { };
 CUfunction *jitc_cuda_aggregate = nullptr;
 
-std::pair<CUmodule, bool> jitc_cuda_compile(const char *buf) {
+std::pair<CUmodule, bool> jitc_cuda_compile(const char *buf, bool release_state_lock) {
     const uintptr_t log_size = 16384;
     char error_log[log_size], info_log[log_size];
-	info_log[0] = '\0';
-	error_log[0] = '\0';
+    info_log[0] = '\0';
+    error_log[0] = '\0';
 
     CUjit_option arg[] = {
         CU_JIT_OPTIMIZATION_LEVEL,
@@ -71,35 +71,37 @@ std::pair<CUmodule, bool> jitc_cuda_compile(const char *buf) {
         (void *) 0
     };
 
-	size_t nargs = sizeof(arg) / sizeof(CUjit_option);
+    size_t nargs = sizeof(arg) / sizeof(CUjit_option);
 
-	CUmodule mod = nullptr;
-	CUresult rv = (CUresult) 0;
+    CUmodule mod = nullptr;
+    CUresult rv = (CUresult) 0;
 
-	for (int i = 0; i < 2; ++i) {
-		{
-			unlock_guard guard(state.lock);
+    for (int i = 0; i < 2; ++i) {
+        if (release_state_lock) {
+            unlock_guard guard(state.lock);
+            rv = cuModuleLoadDataEx(&mod, buf, nargs, arg, argv);
+        } else {
             rv = cuModuleLoadDataEx(&mod, buf, nargs, arg, argv);
         }
 
-		if (rv == CUDA_ERROR_OUT_OF_MEMORY) {
-			if (i == 0)
+        if (rv == CUDA_ERROR_OUT_OF_MEMORY) {
+            if (i == 0)
                 jitc_flush_malloc_cache(true);
-			else
-				cuda_check(rv);
-		} else {
-			break;
-		}
-	}
+            else
+                cuda_check(rv);
+        } else {
+            break;
+        }
+    }
 
-	if (rv != CUDA_SUCCESS)
-		jitc_fail("jit_cuda_compile(): compilation failed. Please see the PTX "
-				  "assembly listing and error message below:\n\n%s\n\n%s",
-				  buf, error_log);
+    if (rv != CUDA_SUCCESS)
+        jitc_fail("jit_cuda_compile(): compilation failed. Please see the PTX "
+                  "assembly listing and error message below:\n\n%s\n\n%s",
+                  buf, error_log);
 
-	bool cache_hit = info_log[0] == '\0';
-	if (!cache_hit)
-		jitc_log(Trace, "Detailed linker output:\n%s", info_log);
+    bool cache_hit = info_log[0] == '\0';
+    if (!cache_hit)
+        jitc_log(Trace, "Detailed linker output:\n%s", info_log);
 
     return { mod, cache_hit };
 }
@@ -257,8 +259,8 @@ bool jitc_cuda_init() {
 
         uncompressed_ptx[kernels_size_uncompressed] = '\0';
 
-		CUmodule m = jitc_cuda_compile(uncompressed_ptx).first;
-		jitc_cuda_module[i] = m;
+        CUmodule m = jitc_cuda_compile(uncompressed_ptx, /* release_state_lock */ false).first;
+        jitc_cuda_module[i] = m;
         free(uncompressed);
 
         #define LOAD(name)                                                       \

src/init.cpp
@@ -336,15 +336,8 @@ ThreadState *jitc_init_thread_state(JitBackend backend) {
 
     if (backend == JitBackend::CUDA) {
         if ((state.backends & (uint32_t) JitBackend::CUDA) == 0) {
-            #if defined(_WIN32)
-                const char *cuda_fname = "nvcuda.dll";
-            #elif defined(__linux__)
-                const char *cuda_fname  = "libcuda.so";
-            #else
-                const char *cuda_fname  = "libcuda.dylib";
-            #endif
-
             delete ts;
+
             if (jitc_cuda_cuinit_result == CUDA_ERROR_NOT_INITIALIZED) {
                 jitc_raise(
                     "jit_init_thread_state(): the CUDA backend hasn't been "
@@ -365,13 +358,20 @@ ThreadState *jitc_init_thread_state(JitBackend backend) {
                            "The specific error message produced by cuInit was\n"
                            "   \"%s\"", msg);
             } else {
-                jitc_raise(
-                    "jit_init_thread_state(): the CUDA backend is inactive "
-                    "because it has not been initialized via jit_init(), or "
-                    "because the CUDA driver library (\"%s\") could not be "
-                    "found! Set the DRJIT_LIBCUDA_PATH environment variable to "
-                    "specify its path.",
-                    cuda_fname);
+#if defined(_WIN32)
+              const char *cuda_fname = "nvcuda.dll";
+#elif defined(__linux__)
+              const char *cuda_fname = "libcuda.so";
+#else
+              const char *cuda_fname = "libcuda.dylib";
+#endif
+              jitc_raise(
+                  "jit_init_thread_state(): the CUDA backend is inactive "
+                  "because it has not been initialized via jit_init(), or "
+                  "because the CUDA driver library (\"%s\") could not be "
+                  "found! Set the DRJIT_LIBCUDA_PATH environment variable to "
+                  "specify its path.",
+                  cuda_fname);
             }
         }
 