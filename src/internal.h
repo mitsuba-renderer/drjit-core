a7f8b7bb8e7003393cce62cc38a518ec1f761e46
Christian Döring <christian.doering@tum.de>
2024-03-22 11:52:13 +0100

    Added kernel_parameter arguments to launch function

--- src/internal.h
+++ src/internal.h
@@ -583,7 +583,10 @@ struct ThreadState {
     ThreadState() = default;
     ThreadState(const ThreadState &other) = default;
 
-    virtual Task *launch(Kernel kernel, uint32_t size) = 0;
+    virtual Task *launch(Kernel kernel, uint32_t size,
+                         std::vector<void *> *kernel_params,
+                         uint32_t kernel_param_count,
+                         const uint8_t *kernel_params_global) = 0;
 
     /// Fill a device memory region with constants of a given type
     virtual void memset_async(void *ptr, uint32_t size, uint32_t isize,
--- src/llvm_ts.h
+++ src/llvm_ts.h
@@ -2,7 +2,10 @@
 
 struct LLVMThreadState: ThreadState{
 
-    Task *launch(Kernel kernel, uint32_t size) override;
+    Task *launch(Kernel kernel, uint32_t size,
+                 std::vector<void *> *kernel_params,
+                 uint32_t kernel_param_count,
+                 const uint8_t *kernel_params_global) override;
     
     /// Fill a device memory region with constants of a given type
     void memset_async(void *ptr, uint32_t size, uint32_t isize,
