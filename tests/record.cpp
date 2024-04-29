2cd7ec34d5ffbe70e37d087994b81ff73b675743
Christian Döring <christian.doering@tum.de>
2024-04-29 17:18:31 +0200

    Added asserts to basic replay test

--- tests/record.cpp
+++ tests/record.cpp
@@ -2,37 +2,48 @@
 #include "test.h"
 
 TEST_BOTH(01_basic_replay) {
-    UInt32 r(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    Recording *recording;
 
-    uint32_t inputs[] = {
-        r.index()
-    };
-
-    jit_record_start(Backend, inputs, 1);
-
-    UInt32 result = r + 1;
-    result.eval();
-
-    uint32_t outputs[] = {
-        result.index()
-    };
-
-    Recording *record = jit_record_stop(Backend, outputs, 1);
-
-
+    jit_log(LogLevel::Info, "Recording:");
     {
-        UInt32 r2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        jit_log(LogLevel::Info, "r2: %s", jit_var_str(r2.index()));
-        
-        uint32_t inputs[] = {
-            r2.index()
-        };
-        uint32_t outputs[1];
-        
-        jit_record_replay(record, inputs, outputs);
+        UInt32 r(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        UInt32 ref(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+        uint32_t inputs[] = {
+            r.index()
+        };
+
+        jit_record_start(Backend, inputs, 1);
+
+        UInt32 result = r + 1;
+        result.eval();
+
+        uint32_t outputs[] = {
+            result.index()
+        };
+
+        recording = jit_record_stop(Backend, outputs, 1);
+        
         jit_log(LogLevel::Info, "result: %s", jit_var_str(outputs[0]));
+        jit_assert(jit_var_all(jit_var_eq(ref.index(), outputs[0])));
     }
 
-    jit_record_destroy(record);
+
+    jit_log(LogLevel::Info, "Replay:");
+    {
+        UInt32 r(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        UInt32 ref(2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+
+        uint32_t inputs[] = {
+            r.index()
+        };
+        uint32_t outputs[1];
+
+        jit_record_replay(recording, inputs, outputs);
+
+        jit_log(LogLevel::Info, "result: %s", jit_var_str(outputs[0]));
+        jit_assert(jit_var_all(jit_var_eq(ref.index(), outputs[0])));
+    }
+
+    jit_record_destroy(recording);
 }
