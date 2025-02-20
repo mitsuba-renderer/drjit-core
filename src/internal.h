d0e81e47a1b41ed7addcd966166cdf76935aff43
Christian Döring <christian.doering@tum.de>
2025-02-20 15:13:35 +0100

    Added symbolic width for freezing means

--- src/internal.h
+++ src/internal.h
@@ -720,6 +720,8 @@ struct ThreadState : public ThreadStateBase {
     virtual void reduce_expanded(VarType vt, ReduceOp op, void *data,
                                  uint32_t exp, uint32_t size) = 0;
 
+    virtual void notify_symbolic_width(uint32_t index, uint32_t width_index);
+
     /// Notify the \c ThreadState that \c jitc_free has been called on a pointer.
     /// This is required for kernel freezing.
     virtual void notify_free(const void *ptr);
