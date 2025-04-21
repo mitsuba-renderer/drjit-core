7909a655a5730227356f1b5ad92aae6460ffd3a8
Wenzel Jakob <wenzel.jakob@epfl.ch>
2025-04-21 16:51:38 +0900

    Scatter elision cleanup
    Dr.Jit can elide scatter operations when their result can no longer be
    referenced by any other operations. The logic to do so, and when
    reference count decreases are needed, was dispersed throughout
    ``eval.cpp``. This commit simplifies the underlying code.

--- src/llvm_eval.cpp
+++ src/llvm_eval.cpp
@@ -953,6 +953,7 @@ static void jitc_llvm_render(Variable *v) {
                 }
             }
             break;
+
         case VarKind::Scatter:
             if (v->literal)
                 jitc_llvm_render_scatter_reduce(v, a0, a1, a2, a3);
