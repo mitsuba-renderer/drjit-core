4dd6457190379b2916cf0c914282d6226127aee3
Wenzel Jakob <wenzel.jakob@epfl.ch>
2026-05-08 19:38:32 +0200

    updated kernel LZ4 file

--- resources/kernels.h
+++ resources/kernels.h
@@ -18,8 +18,8 @@ static const size_t kernels_dict_size_uncompressed = 65536;
 static const size_t kernels_dict_size_compressed = 19638;
 
 extern const char kernels_75[];
-static const size_t kernels_75_size_uncompressed = 5371039;
-static const size_t kernels_75_size_compressed = 1036364;
+static const size_t kernels_75_size_uncompressed = 5374370;
+static const size_t kernels_75_size_compressed = 1037165;
 
 static const char *kernels_list =
     "compress_small,"
